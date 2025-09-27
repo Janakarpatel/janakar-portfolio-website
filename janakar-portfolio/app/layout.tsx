ff80f86eb7866d263b19a55be56a136e2acad75e
Pateljanakar <janakar.pict20@sot.pdpu.ac.in>
2025-09-27 19:05:56 +0530

    Adding the initial version of the next js portfolio website.

---
+++ janakar-portfolio/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ 
+  subsets: ['latin'],
+  weight: ['400', '600']
+})
+
+export const metadata: Metadata = {
+  title: 'Janakar Patel - Portfolio',
+  description: 'Data, AI/ML Software + Research Portfolio',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link 
+          rel="stylesheet" 
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" 
+        />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
